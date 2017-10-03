fd81c0fba6ac59514d1c1f9ca8ac042a2c84066e
Lu Xuzheng <466532329@qq.com>
2017-10-03 18:08:44 +0800

    DNN

--- models.py
+++ models.py
@@ -1976,8 +1976,6 @@ class DeepNeuralNetworks:
             # # Activate function
             # fc = tf.sigmoid(fc_layer)
 
-            x_tensor = tf.cast(x_tensor, tf.float32)
-
             fc = tf.contrib.layers.fully_connected(x_tensor,
                                                    num_outputs,
                                                    activation_fn=tf.nn.sigmoid,
@@ -1985,9 +1983,8 @@ class DeepNeuralNetworks:
                                                    # stddev=2.0 / math.sqrt(x_shape[1])),
                                                    weights_initializer=tf.contrib.layers.xavier_initializer(dtype=tf.float64,
                                                                                                             seed=self.dnn_seed),
-                                                   normalizer_fn=tf.contrib.layers.batch_norm,
-                                                   biases_initializer=None
-                                                   # biases_initializer=tf.zeros_initializer(dtype=tf.float64)
+                                                   # normalizer_fn=tf.contrib.layers.batch_norm,
+                                                   biases_initializer=tf.zeros_initializer(dtype=tf.float64)
                                                    )
 
             tf.summary.histogram('fc_layer', fc)
