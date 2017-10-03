84cc775843c2d4c1ff962007248534fd6f2519fd
Lu Xuzheng <466532329@qq.com>
2017-10-03 17:45:50 +0800

    DNN

--- models.py
+++ models.py
@@ -1976,6 +1976,8 @@ class DeepNeuralNetworks:
             # # Activate function
             # fc = tf.sigmoid(fc_layer)
 
+            x_tensor = tf.cast(x_tensor, tf.float64)
+
             fc = tf.contrib.layers.fully_connected(x_tensor,
                                                    num_outputs,
                                                    activation_fn=tf.nn.sigmoid,
