7c8a8f5c2ea66f6ba5072314f0b15e0a0649f51f
Lu Xuzheng <466532329@qq.com>
2017-10-10 21:01:12 +0800

    Fix bug

--- main.py
+++ main.py
@@ -539,11 +539,9 @@ class ChampionModel:
 
         print('Start training LightGBM...')
 
-        print(train_seed, ',', cv_seed)
-
-        # LGBM.train(pred_path, loss_log_path, n_valid=4, n_cv=20, n_era=20, train_seed=train_seed,
-        #            cv_seed=cv_seed, parameters=lgb_parameters, show_importance=False,
-        #            save_csv_log=True, csv_idx=idx, cv_generator=cv_generator)
+        LGBM.train(pred_path, loss_log_path, n_valid=4, n_cv=20, n_era=20, train_seed=train_seed,
+                   cv_seed=cv_seed, parameters=lgb_parameters, show_importance=False,
+                   save_csv_log=True, csv_idx=idx, cv_generator=cv_generator)
 
 
 class GridSearch:
