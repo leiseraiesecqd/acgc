bb0876720fb1ff03af232d2b1e3fa5202bc480d6
Lu Xuzheng <466532329@qq.com>
2017-10-17 01:28:02 +0800

    PrejudgeBinary

--- main.py
+++ main.py
@@ -1132,7 +1132,7 @@ class PrejudgeTraining:
 
         era_training_params = {'application': 'binary',
                                'boosting': 'gbdt',               # gdbt,rf,dart,goss
-                               'learning_rate': 0.02,            # default=0.1
+                               'learning_rate': 0.05,            # default=0.1
                                'num_leaves': 88,                 # default=31       <2^(max_depth)
                                'max_depth': 10,                  # default=-1
                                'min_data_in_leaf': 2500,         # default=20       reduce over-fit
