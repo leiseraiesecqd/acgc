2c10ffc84841425b3386083a82b58ddb706d9062
Lu Xuzheng <466532329@qq.com>
2017-10-19 13:29:55 +0800

    Grid searching xgb subsample

--- main.py
+++ main.py
@@ -1680,7 +1680,7 @@ def auto_grid_search():
         Automatically Grid Searching
     """
 
-    parameter_grid = ['max_depth', (7, 8, 9, 10)]
+    parameter_grid = ['subsample', (0.6)]
     n_epoch = 100
 
     for param in parameter_grid[1]:
