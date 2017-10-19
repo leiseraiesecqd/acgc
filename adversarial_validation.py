a10ee4da9574cfeb869a14bda8bc1db8fcccce90
Lu Xuzheng <466532329@qq.com>
2017-10-20 02:50:46 +0800

    Modify params

--- adversarial_validation.py
+++ adversarial_validation.py
@@ -61,8 +61,8 @@ class AdversarialValidation(object):
                                                           activation_fn=tf.nn.sigmoid,
                                                           weights_initializer=weights_initializer,
                                                           weights_regularizer=weights_reg,
-                                                          normalizer_fn=normalizer_fn,
-                                                          normalizer_params=normalizer_params,
+                                                          # normalizer_fn=normalizer_fn,
+                                                          # normalizer_params=normalizer_params,
                                                           biases_initializer=tf.zeros_initializer(dtype=tf.float32))
 
                 tf.summary.histogram('fc_layer', layer)
@@ -80,8 +80,8 @@ class AdversarialValidation(object):
                                                           activation_fn=None,
                                                           weights_initializer=weights_initializer,
                                                           weights_regularizer=weights_reg,
-                                                          normalizer_fn=normalizer_fn,
-                                                          normalizer_params=normalizer_params,
+                                                          # normalizer_fn=normalizer_fn,
+                                                          # normalizer_params=normalizer_params,
                                                           biases_initializer=tf.zeros_initializer(dtype=tf.float32))
 
                 tf.summary.histogram('fc_layer', layer)
@@ -127,8 +127,8 @@ class AdversarialValidation(object):
                                                           activation_fn=tf.nn.sigmoid,
                                                           weights_initializer=weights_initializer,
                                                           weights_regularizer=weights_reg,
-                                                          normalizer_fn=normalizer_fn,
-                                                          normalizer_params=normalizer_params,
+                                                          # normalizer_fn=normalizer_fn,
+                                                          # normalizer_params=normalizer_params,
                                                           biases_initializer=tf.zeros_initializer(dtype=tf.float32))
 
                 tf.summary.histogram('fc_layer', layer)
