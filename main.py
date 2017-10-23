8ae1338e95ed524fa0feb8cbc23dbd4b4957fd2e
Lu Xuzheng <466532329@qq.com>
2017-10-23 17:14:16 +0800

    Gridsearching bagging_fraction num_leaves

--- main.py
+++ main.py
@@ -1715,11 +1715,11 @@ def auto_grid_search():
     """
         Automatically Grid Searching
     """
-    pg_1 = ['feature_fraction', (0.5, 0.6, 0.7, 0.8, 0.9)]
-    # pg_2 = ['bagging_fraction', (0.6, 0.7, 0.8, 0.9)]
+    # pg_1 = ['feature_fraction', (0.5, 0.6, 0.7, 0.8, 0.9)]
+    pg_2 = ['bagging_fraction', (0.6, 0.7, 0.8, 0.9)]
     # pg_3 = ['bagging_freq', (1, 2, 3, 4, 5)]
-    pg_4 = ['max_depth', (7, 8, 9, 10)]
-    # pg_5 = ['num_leaves', (70, 75, 80, 85, 90)]
+    # pg_4 = ['max_depth', (7, 8, 9, 10)]
+    pg_5 = ['num_leaves', (70, 75, 80, 85, 90)]
     # pg_6 = ['min_data_in_bin', (1, 3, 5, 7, 9)]
 
     parameter_grid_list = [pg_1, pg_4]
