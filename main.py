f6225f68d4d5dd5b08dad1e691e6524f55060490
Lu Xuzheng <466532329@qq.com>
2017-10-23 16:20:38 +0800

    Auto Gridsearch LGB

--- main.py
+++ main.py
@@ -1715,7 +1715,7 @@ def auto_grid_search():
     """
         Automatically Grid Searching
     """
-    parameter_grid = ['feature_fraction', (0.5, 0.6, 0.7, 0.8, 0.9)]
+    parameter_grid = ['bagging_fraction', (0.6, 0.7, 0.8, 0.9)]
     n_epoch = 200
 
     for param in parameter_grid[1]:
