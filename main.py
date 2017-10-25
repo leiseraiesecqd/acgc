22b0ff5f821f7cbfd955ea16a6bdd2803e3804ba
Lu Xuzheng <466532329@qq.com>
2017-10-26 00:42:19 +0800

    Auto Train XGB

--- main.py
+++ main.py
@@ -250,8 +250,8 @@ class SingleModel:
                       'max_depth': 9,                   # default=6
                       'min_child_weight': 18,           # default=1，建立每个模型所需最小样本权重和
                       'subsample': 0.9,                 # 建立树模型时抽取子样本占整个样本的比例
-                      'colsample_bytree': 0.8,          # 建立树时对特征随机采样的比例
-                      'colsample_bylevel': 0.7,
+                      'colsample_bytree': 0.7,          # 建立树时对特征随机采样的比例
+                      'colsample_bylevel': 0.6,
                       'lambda': 0,
                       'alpha': 0,
                       'early_stopping_rounds': 30,
@@ -1814,7 +1814,7 @@ class Training:
         """
             Train Single Model
         """
-        self.train_single_model('lgb', train_seed, cv_seed, options=options)
+        # self.train_single_model('lgb', train_seed, cv_seed, options=options)
         # self.train_single_model('prejudge_b', train_seed, cv_seed, load_pickle=False, options=options)
 
         """
@@ -1841,7 +1841,7 @@ class Training:
         """
             Auto Train
         """
-        # self.auto_train('xgb', n_epoch=200, options=options)
+        self.auto_train('xgb', n_epoch=200, options=options)
         # self.auto_train('stack_t', n_epoch=1, stack_final_epochs=10, options=options)
 
         print('======================================================')
