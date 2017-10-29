f9e6cd9c55fede46caa3d0a16b9a7878c980b9e7
Lu Xuzheng <466532329@qq.com>
2017-10-29 21:03:27 +0800

    Add train_args

--- main.py
+++ main.py
@@ -1975,8 +1975,8 @@ class Training:
         """
             Train Single Model
         """
-        self.train_single_model('lgb', train_seed, cv_seed, reduced_feature_list=reduced_feature_list,
-                                train_args=train_args, train_options=train_options)
+        # self.train_single_model('lgb', train_seed, cv_seed, reduced_feature_list=reduced_feature_list,
+        #                         train_args=train_args, train_options=train_options)
         # self.train_single_model('prejudge_b', train_seed, cv_seed, load_pickle=False,
         #                         reduced_feature_list=reduced_feature_list,
         #                         train_args=train_args, train_options=train_options)
@@ -2016,7 +2016,7 @@ class Training:
         """
             Auto Train
         """
-        # self.auto_train('lgb', n_epoch=10000, train_args=train_args, train_options=train_options)
+        self.auto_train('lgb', n_epoch=10000, train_args=train_args, train_options=train_options)
         # self.auto_train('stack_t', n_epoch=2, stack_final_epochs=10,
         #                 reduced_feature_list=reduced_feature_list, train_args=train_args, train_options=train_options)
 
