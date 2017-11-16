b88a370f485fa3191a59ad88fc327bbdb2a8c753
Lu Xuzheng <466532329@qq.com>
2017-11-16 18:42:27 +0800

    Grid search cv arguments of XGB forward increase log cv_weights postscale

--- boost_round_train.py
+++ boost_round_train.py
@@ -206,7 +206,7 @@ class Training:
                       'save_cv_pred': False,
                       'save_cv_prob_train': False,
                       'save_csv_log': True,
-                      'append_info': 'forward_increase_postscale'}
+                      'append_info': 'forward_increase_log_postscale'}
 
         """
             Cross Validation Arguments
