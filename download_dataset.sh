34a7f44b97570c9e8caaeeec8e9d46213fdf24af
Lu Xuzheng <466532329@qq.com>
2017-11-27 00:02:11 +0800

    New dataset

--- download_dataset.sh
+++ download_dataset.sh
@@ -9,12 +9,12 @@ rm -rf checkpoints
 git pull
 mkdir inputs
 cd inputs/
-wget http://static1.challenger.ai/ai_challenger_stock_train_20171117.zip
-wget http://static1.challenger.ai/ai_challenger_stock_test_20171117.zip
-unzip ai_challenger_stock_test_20171117.zip
-unzip ai_challenger_stock_train_20171117.zip
-mv data/20171117/ai_challenger_stock_test_20171117/stock_test_data_20171117.csv ./
-mv data/20171117/ai_challenger_stock_train_20171117/stock_train_data_20171117.csv ./
+wget http://static1.challenger.ai/ai_challenger_stock_train_20171125.zip
+wget http://static1.challenger.ai/ai_challenger_stock_test_20171125.zip
+unzip ai_challenger_stock_test_20171125.zip
+unzip ai_challenger_stock_train_20171125.zip
+mv data/20171125/ai_challenger_stock_test_20171125/stock_test_data_20171125.csv ./
+mv data/20171125/ai_challenger_stock_train_20171125/stock_train_data_20171125.csv ./
 rm *.zip
 rm -rf data/
 cd ..
