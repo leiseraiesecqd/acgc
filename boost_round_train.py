41579b6a487c47603583c7a77ea183c732c176c3
Lu Xuzheng <466532329@qq.com>
2017-11-30 22:17:35 +0800

    GB

--- boost_round_train.py
+++ boost_round_train.py
@@ -277,7 +277,7 @@ class Training:
                       'save_cv_pred': False,
                       'save_cv_prob_train': False,
                       'save_csv_log': True,
-                      'append_info': 'forward_window_postscale'}
+                      'append_info': 'forward_window_postscale_params'}
 
         """
             Cross Validation Arguments
@@ -318,11 +318,18 @@ class Training:
                    #  ['cv_weights', cv_weights_list]]
                    [
                     ['max_depth', [10]],
-                    ['min_child_weight', (5, 6, 7, 8)],
-                    ['subsample', (0.86, 0.87)],
-                    ['colsample_bytree', (0.9, 0.91)],
-                    ['colsample_bylevel', [0.68], [0.69], [0.7]]
+                    ['min_child_weight', (5, 6, 7, 8, 9, 10, 11, 12)],
+                    ['subsample', (0.86, 0.87, 0.88, 0.90)],
+                    ['colsample_bytree', [0.9]],
+                    ['colsample_bylevel', (0.68, 0.7, 0.72, 0.74)]
                     ]
+                   [
+                    ['max_depth', [9]],
+                    ['min_child_weight', (5, 6, 7, 8, 9, 10, 11, 12)],
+                    ['subsample', (0.87, 0.88, 0.89, 0.90, 0.91)],
+                    ['colsample_bytree', [0.9]],
+                    ['colsample_bylevel', (0.7, 0.73, 0.76, 0.79)]
+                   ]
                    # [['learning_rate', [0.003]]]
                    ]
         train_seed_list = [999]
