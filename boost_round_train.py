9cb93137879340d6a9f42f4efecd916e4b1f87ba
Lu Xuzheng <466532329@qq.com>
2017-11-30 20:19:29 +0800

    Grid search higher mcw

--- boost_round_train.py
+++ boost_round_train.py
@@ -272,9 +272,9 @@ class Training:
                       'use_custom_obj': False,
                       'show_importance': False,
                       'show_accuracy': False,
-                      'save_final_pred': True,
+                      'save_final_pred': False,
                       'save_final_prob_train': False,
-                      'save_cv_pred': True,
+                      'save_cv_pred': False,
                       'save_cv_prob_train': False,
                       'save_csv_log': True,
                       'append_info': 'forward_window_postscale'}
@@ -316,20 +316,20 @@ class Training:
                    # [['n_cv', n_cv_list],
                    #  ['valid_rate', valid_rate_list],
                    #  ['cv_weights', cv_weights_list]]
-                   # [
-                   #  ['max_depth', (9, 10)],
-                   #  ['min_child_weight', (2, 3, 4)],
-                   #  ['subsample', (0.86, 0.87, 0.88, 0.9, 0.91)],
-                   #  ['colsample_bytree', (0.86, 0.87, 0.88, 0.89, 0.9)],
-                   #  ['colsample_bylevel', (0.69, 0.7), (0.72, 0.74), (0.76, 0.78, 0.8), [0.81]]
-                   #  ]
-                   [['learning_rate', [0.003]]]
+                   [
+                    ['max_depth', [10]],
+                    ['min_child_weight', (5, 6, 7, 8)],
+                    ['subsample', (0.86, 0.87)],
+                    ['colsample_bytree', (0.9, 0.91)],
+                    ['colsample_bylevel', [0.68], [0.69], [0.7]]
+                    ]
+                   # [['learning_rate', [0.003]]]
                    ]
         train_seed_list = [999]
         cv_seed_list = [95]
-        TM.auto_train_boost_round('xgb', num_boost_round=90, n_epoch=1, full_grid_search=True,
+        TM.auto_train_boost_round('xgb', num_boost_round=100, n_epoch=1, full_grid_search=True,
                                   use_multi_group=False, train_seed_list=train_seed_list, cv_seed_list=cv_seed_list,
-                                  base_parameters=base_parameters, parameter_grid_list=pg_list, save_final_pred=True,
+                                  base_parameters=base_parameters, parameter_grid_list=pg_list, save_final_pred=False,
                                   reduced_feature_list=reduced_feature_list, train_args=train_args, cv_args=cv_args)
 
         """Train Different Rounds"""
